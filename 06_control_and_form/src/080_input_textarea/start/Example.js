bce87fa9c0685f96c8f25c48754108665596557d
ramenumai <taiki1151001@icloud.com>
2023-03-19 17:03:39 +0900

    input or textarea

--- 06_control_and_form/src/080_input_textarea/start/Example.js
+++ 06_control_and_form/src/080_input_textarea/start/Example.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [val, setVal] = useState("");
+  const clear = () => setVal("");
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
+    <>
+      <label htmlFor="123">ラベル</label>
       <br />
-      練習用に使ってください！
-    </p>
+
+      <input
+        type="text"
+        id="123"
+        placeholder="こんにちは"
+        value={val}
+        onChange={(e) => {
+          setVal(e.target.value);
+        }}
+      />
+
+      <textarea
+        name=""
+        id="123"
+        placeholder="こんにちは"
+        value={val}
+        onChange={(e) => {
+          setVal(e.target.value);
+        }}
+      />
+      <h3>{val}</h3>
+      <button onClick={clear}>クリア</button>
+    </>
   );
 };
 
