f352cb433c86f4a3fd78b01983e23f8f8ce08bcc
ramenumai <taiki1151001@icloud.com>
2023-03-24 19:32:51 +0900

    radio button

--- 06_control_and_form/src/090_radio/start/Example.js
+++ 06_control_and_form/src/090_radio/start/Example.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [radioVal, SetradioVal] = useState('Apple');
+  // const onChange = (e) => SetradioVal(e.target.value);
+
+  const RADIO_COLLECTION = ['Apple', 'Banana', 'Cherry'];
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      {RADIO_COLLECTION.map((value) => {
+        return (
+          <label key = {value}>
+            <input
+              type="radio"
+              value={value}
+              checked={radioVal === value}
+              onChange={(e) => SetradioVal(e.target.value)}
+            />
+            {value}
+          </label>
+        );
+      })}
+      <h3>私は{radioVal}が食べたいです</h3>
+    </>
   );
 };
 
